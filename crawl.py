cf5d2e833055e53b1080ac65d9260d9278553e58
Uzzi <Ujjwalmishraa24@gmail.com>
2026-06-20 13:28:45 +0530

    content extraciton

--- crawl.py
+++ crawl.py
@@ -2,7 +2,7 @@
 from urllib.parse import urlsplit, urljoin
 from bs4 import BeautifulSoup, Tag
 
-
+# for getting url in a normal form.
 def normalize_url(url):
     url_parts = urlsplit(url)
     netloc = url_parts.netloc
@@ -10,7 +10,10 @@ def normalize_url(url):
     normalized_url = netloc + path
     return normalized_url
 
-print(normalize_url("https://www.youtube.com/watch?v=QDia3e12czc"))
+# print(normalize_url("https://www.youtube.com/watch?v=QDia3e12czc"))
+
+
+# for getting the content of the html tags like h1, h2, and p.
 def get_heading_from_html(html: str) -> str:
     soup = BeautifulSoup(html, "html.parser")
     para = soup.find_all("p")
@@ -21,3 +24,46 @@ def get_heading_from_html(html: str) -> str:
     h2s = [h.get_text() for h in h2]
     return paragraphs, h1s, h2s
 
+
+# for getting the urls from the html body and normalise them to absolute urls
+def get_urls_from_html(html: str, base_url: str) -> list:
+        soup = BeautifulSoup(html, "html.parser")
+        link =  soup.find_all("a")
+        urls = []
+        for l in link:
+            href = l.get("href")
+            if href:
+                urls.append(urljoin(base_url, href))
+        return urls
+
+
+# for getting the image urls from the html body and normalise them to absolute urls
+def get_images_from_html(html :str, base_url : str) -> list:
+    soup = BeautifulSoup(html, "html.parser")
+    image =soup.find_all("img")
+    images = []
+    for i in image:
+        src = i.get("src")
+        if src:
+            images.append(urljoin(base_url, src))
+    return images
+
+# all data combined to see as a result till now.
+def extract_page_data(html: str, page_url: str) -> dict:
+    page_data = {
+        "url": normalize_url(page_url),
+        "content of page": get_heading_from_html(html),
+        "outgoing urls": get_urls_from_html(html, page_url),
+        "image urls": get_images_from_html(html, page_url)
+    }
+    return page_data
+
+input_url = "https://crawler-test.com"
+input_body = '''<html><body>
+        <h1>Test Title</h1>
+        <p>This is the first paragraph.</p>
+        <a href="/link1">Link 1</a>
+        <img src="/image1.jpg" alt="Image 1">
+    </body></html>'''
+
+print(extract_page_data(input_body, input_url))
--- test_crawl.py
+++ test_crawl.py
@@ -1,7 +1,9 @@
 import unittest
-from crawl import normalize_url , get_heading_from_html
-
+from crawl import normalize_url , get_heading_from_html , get_urls_from_html , get_images_from_html, extract_page_data
 class TestCrawl(unittest.TestCase):
+
+
+# test for normalising similar urls  
     def test_normalize_url(self):
         input_url = "https://www.youtube.com/watch?v=QDia3e12czc"
         actual = normalize_url(input_url)
@@ -9,6 +11,7 @@ class TestCrawl(unittest.TestCase):
         self.assertEqual(actual, expected)
 
 
+# written to get the content of the html tags like h1, h2, and p.
     def test_html_parsing(self):
         html = '''<!DOCTYPE html>
         <html>
@@ -25,6 +28,80 @@ class TestCrawl(unittest.TestCase):
         actual = get_heading_from_html(html)
         expected = (['This is a paragraph.'], ['This is a Heading'], [])
         self.assertEqual(actual, expected)
+        
+# 2 below tests are below for absolute and relative url parse.
+    def test_get_urls_from_html_absolute(self):
+        input_url = "https://crawler-test.com"
+        input_body = '<html><body><a href="https://crawler-test.com"><span>Boot.dev</span></a></body></html>'
+        actual = get_urls_from_html(input_body, input_url)
+        expected = ["https://crawler-test.com"]
+        self.assertEqual(actual, expected)
+
+    def test_get_urls_from_html_relative(self):
+        input_url = "https://crawler-test.com"
+        input_body = '<html><body><a href="/"><span>Boot.dev</span></a></body></html>'
+        actual = get_urls_from_html(input_body, input_url)
+        expected = ["https://crawler-test.com/"]
+        self.assertEqual(actual, expected)
+
+# 2 below tests are for absolute and relative image url parse.
+    def test_get_images_from_html_relative(self):
+        input_url = "https://crawler-test.com"
+        input_body = '<html><body><img src="/logo.png" alt="Logo"></body></html>'
+        actual = get_images_from_html(input_body, input_url)
+        expected = ["https://crawler-test.com/logo.png"]
+        self.assertEqual(actual, expected)
+
+    def test_get_images_from_html_absolute(self):
+        input_url = "https://crawler-test.com"
+        input_body = '<html><body><img src="https://crawler-test.com/logo.png" alt="Logo"></body></html>'
+        actual = get_images_from_html(input_body, input_url)
+        expected = ["https://crawler-test.com/logo.png"]
+        self.assertEqual(actual, expected)
+
+
+
+    # def test_extract_page_data_basic(self):
+    #     input_url = "https://crawler-test.com"
+    #     input_body = '''<html><body>
+    #         <h1>Test Title</h1>
+    #         <p>This is the first paragraph.</p>
+    #         <a href="/link1">Link 1</a>
+    #         <img src="/image1.jpg" alt="Image 1">
+    #     </body></html>'''
+    #     actual = extract_page_data(input_body, input_url)
+    #     expected = {
+    #         "url": "https://crawler-test.com",
+    #         "heading": "Test Title",
+    #         "content of page": ("This is the first paragraph.", ["Test Title"], []),
+    #         "outgoing urls": ["https://crawler-test.com/link1"],
+    #         "image urls": ["https://crawler-test.com/image1.jpg"]
+    #     }
+    #     self.assertEqual(actual, expected)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     unittest.main()
